02acd3f60ace4dab94b26b0dcfe6a287618b433d
Alexis Cerio <alexis111@orange.fr>
2024-04-02 17:35:32 +0200

    Add Description & Authors fields in the user confirming form

--- src/components/TitleConfirmModal/index.js
+++ src/components/TitleConfirmModal/index.js
@@ -4,7 +4,7 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 
-const TitleConfirmModal = ({ show, handleClose, title, handleConfirm, setTitle}) => {
+const TitleConfirmModal = ({ show, handleClose, title, handleConfirm, setTitle, authors, setAuthors, description, setDescription}) => {
 
     return (
         <>
@@ -15,11 +15,40 @@ const TitleConfirmModal = ({ show, handleClose, title, handleConfirm, setTitle})
                 <Modal.Body>
                     <Form>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Label>Please type the tile of your research again</Form.Label>
+                            <Form.Label>Please enter the title of your research:</Form.Label>
                             <Form.Control
                                 autoFocus
                                 value={title}
-                                onChange={(e) => setTitle(e.target.value)}
+                                onChange={(e) => {
+                                    if (e.target.value.length <= 30) {
+                                        setTitle(e.target.value)
+                                    }
+                                  }
+                                }
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+                            <Form.Label>Please enter a short description of your research:</Form.Label>
+                            <Form.Control
+                                value={description}
+                                onChange={(e) => {
+                                    if (e.target.value.length <= 150) {
+                                        setDescription(e.target.value)
+                                    }
+                                  }
+                                }
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput3">
+                            <Form.Label>Please enter the name(s) of the author(s):</Form.Label>
+                            <Form.Control
+                                value={authors}
+                                onChange={(e) => {
+                                    if (e.target.value.length <= 58) {
+                                        setAuthors(e.target.value)
+                                    }
+                                  }
+                                }
                             />
                         </Form.Group>
                     </Form>
